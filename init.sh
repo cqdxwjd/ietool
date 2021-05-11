76228964e16214637b2c5e6a116c0593fb7243ba
shuihuo <cqdxwjd@163.com>
2021-05-11 18:38:47 +0800

    初始化安装pip

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #bin/bash
 
+sudo wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+sudo python get-pip.py
 sudo yum install -y python-devel cyrus-sasl-plain  cyrus-sasl-devel  cyrus-sasl-gssapi cyrus-sasl cyrus-sasl-lib gcc-c++ gcc
-
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
