7b4b04ff4aae0b5873763be9367edbaa39aef53c
shishuihuo <cqdxwjd@gmail.com>
2021-05-14 14:51:45 +0800

    1、修改thrift版本为0.9.3。 2、增加了项目依赖的第三方python离线包及安装步骤，可在没有外网的情况下使用。

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 sudo wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
 sudo python get-pip.py
-sudo yum install -y python-devel cyrus-sasl-plain  cyrus-sasl-devel  cyrus-sasl-gssapi cyrus-sasl cyrus-sasl-lib gcc-c++ gcc
+# 由于linux rpm包依赖太过复杂，离线安装工作量巨大，这一步最好采用在线安装。
+sudo yum install -y python-devel cyrus-sasl-plain  cyrus-sasl-devel  cyrus-sasl-gssapi cyrus-sasl cyrus-sasl-lib gcc-c++ gcc unzip
 sudo pip install -r requirements.txt
